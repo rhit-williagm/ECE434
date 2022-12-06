5585ce53048460dab0f9fadefa4e17c2c33bc7f5
Gaven M. Williams <williagm@rose-hulman.edu>
2022-12-06 21:52:30 +0000

    etchasketch instructions

--- hw01/etchasketch.py
+++ hw01/etchasketch.py
@@ -1,1 +1,18 @@
 #!/usr/bin/env python3
+
+print()
+print("Welcome to Gaven's Super Etch-A-Sketch Simulator!")
+print("Use the arrow keys to move your cursor.")
+print("Channel your inner artist to create a picture like so:")
+print("   0 1 2 3 4 5 6 7")
+print("0:     x x x x    ")
+print("1:   x         x  ")
+print("2: x   x     x   x")
+print("3: x             x")
+print("4: x   x     x   x")
+print("5: x     x x     x")
+print("6:   x         x  ")
+print("7:     x x x x")
+
+
+
